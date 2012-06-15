a4f1b27dabc5945ac36145d70c03de2cd9ed1e6b
Aluriak <lucas.bourneuf@laposte.net>
2012-06-15 11:20:21 +0200

    Commentaires et explications plus claires pour les convertions

--- src_Gravastre/main.h
+++ src_Gravastre/main.h
@@ -32,9 +32,10 @@
     #define FILE_INI "Fichiers/INI.Alk"    // contient les valeurs utilisateurs
     #define FILE_AST "Fichiers/AST.Alk"    // contient les astres
     #define FILE_OUT "Fichiers/Out.Alk"	// deviens sortie standard si besoin
-// CONVERTION DE VALEURS (multiplier les valeurs réelles par ces valeurs)
+// CONVERTION DE VALEURS 
+// (multiplier les valeurs réelles par ces valeurs pour avoir les valeurs de calcul du programme)
     #define CONVERTION_KG 1e-17 // car tous les poids sont  divisé par 1e17 
-    #define CONVERTION_KM 1/500000000 // car 300px = 1 ua = 150 000 000 000 m
+    #define CONVERTION_METRE 500000000 // car 300px = 150 000 000 000 m
 
 
 /*
